8798815a7df6eebcc06e8513a40f9b4ff8c2e630
Avula Sai Raghu Kiran <106566627+sairaghukiran14@users.noreply.github.com>
2023-06-08 20:23:09 +0530

    Update queries.js

--- queries.js
+++ queries.js
@@ -7,100 +7,190 @@
 // company_drives
 // mentors
 
-
 use zenclassprogramme
 
 db.createCollection("users")
 
+
 db.users.insertOne({
-  user_name: "John Doe",
-  user_email: "johndoe@example.com",
-  user_password: "password123",
-  user_role: "student"
+  user_id:"U100",
+  user_name: "Raghu kiran",
+  user_email: "raghukiran@example.com",
+  user_password: "Raghu@123",
+  user_mentor_id: "M173"
 })
 
 db.createCollection("codekata")
 
 db.codekata.insertOne({
-  title: "Palindrome Check",
-  description: "Write a function to check if a string is a palindrome.",
-  difficulty: "Easy"
+  user_id:"U100",
+  user_coding_solutions:12
 })
 
 db.createCollection("attendance")
 
 db.attendance.insertOne({
-  userId: ObjectId("615c6f2b41c662a9d0fc9d21"),
-  date: ISODate("2023-05-30"),
-  present: true
+  user_Id: "U100",
+  topic_id:"T100",
+  user_appearance:true
 })
 
 db.createCollection("topics")
 
 db.topics.insertOne({
-  title: "Introduction to Algorithms",
-  description: "An overview of algorithms and their importance in computer science."
+    topic_id: "T100",
+    topic_tile: "HTML",
+    topic_description:"Markup language",
+    topic_date: ISODate("2023-06-08"),
 })
 
 db.createCollection("tasks")
 
 db.tasks.insertOne({
-  title: "Implement Binary Search",
-  description: "Write a function to implement the binary search algorithm.",
-  deadline: ISODate("2023-06-15")
+    task_id: "TA100",
+    topic_id: "T100",
+    user_id: "U100",
+    task_title: "HTML task",
+    task_due_date: ISODate("2023-06-08"),
+    task_submitted: true,
 })
 
 db.createCollection("company_drives")
 
 db.company_drives.insertOne({
-  company: "ABC Corp",
-  date: ISODate("2023-07-01"),
-  location: "Virtual",
-  description: "A recruitment drive conducted by ABC Corp."
+     user_id: "T100",
+    company_drive_date: ISODate("2023-06-08"),
+    company_name: "Google"
 })
 
 db.createCollection("mentors")
 
 db.mentors.insertOne({
-  name: "Jane Smith",
-  email: "janesmith@example.com",
-  expertise: ["Web Development", "Database Management"]
+  mentor_id: "M173",
+    mentor_name: "Arun",
+    mentor_email: "Arun@gmail.com",
+  mentor_expertise: ["Web Development", "Database Management"]
 })
 
-
 // Find all the topics and tasks which are thought in the month of October
-db.topics.aggregate([{$match: {createdAt: {$gte: ISODate("2023-10-01"),$lt: ISODate("2023-11-01")}}},{$lookup: {from: "tasks",localField: "_id",foreignField: "topicId",as: "tasks" }}])
+
+db.topics.aggregate(
+    [
+        {
+            $match: {
+                topic_date: {
+                    $gte: ISODate("2023-10-01"),
+                    $lt: ISODate("2023-10-31")
+                }
+            }
+        },
+        {
+            $lookup: {
+                from: "tasks",
+                localField: "topic_id",
+                foreignField: "task_id",
+                as: "tasks" 
+            }
+        }
+    ]
+    )
 
 // Find all the company drives which appeared between 15 oct-2020 and 31-oct-2020
 
-db.company_drives.find({date: {$gte: ISODate("2020-10-15"),$lte: ISODate("2020-10-31")}})
+    db.company_drives.find(
+        {
+            drive_date: {
+                            $gte: ISODate("2020-10-15"),
+                            $lte: ISODate("2020-10-31"),
+                        }
+        }
+        );
 
 // Find all the company drives and students who are appeared for the placement.
 
-db.company_drives.aggregate([{$lookup: {from: "users",localField: "_id",foreignField: "companyId",as: "students"}}])
-
+db.company_drives.aggregate(
+    [
+        {
+            $lookup: {
+                        from: "users",
+                        localField: "user_id",
+                        foreignField: "user_id",
+                        as: "users",
+                    },
+        }
+    ]
+    );
 
 // Find the number of problems solved by the user in codekata
 
-db.codekata.aggregate([{$match: {"user": "John Doe" }},{$group: {_id: "$user",totalProblemsSolved: { $sum: 1 }}}])
+db.codekata.aggregate(
+    [
+        {
+            $lookup: 
+            {
+                from: "users",
+                localField: "user_id",
+                foreignField: "user_id",
+                as: "users",
+            },
+        },
+        {
+            $project: 
+            {
+                user_coding_solutions: 12,
+                user_name: "Raghu kiran",
+                user_id: "U100",
+            },
+  },
+]);
 
 // Find all the mentors with who has the mentee's count more than 15
+db.mentors.aggregate(
+    [
+        {
+            $lookup: 
+            {
+                from: "mentors",
+                localField: "mentor_id",
+                foreignField: "mentor_id",
+                as: "mentors"
+            }
+        }
+        ,
+        {
+            $match: {
+                $expr: {
+                     $gt: [{ $size: "$mentors" }, 15] 
+                    }
+                }
+        }
+        ])
 
-db.mentors.aggregate([{$lookup: {from: "mentees",localField: "_id",foreignField: "mentorId",as: "mentees"}},{$match: {$expr: { $gt: [{ $size: "$mentees" }, 15] }}  }])
+ // Find the number of users who are absent and task is not submitted  between 15 oct-2020 and 31-oct-2020
 
-// Find the number of users who are absent and task is not submitted  between 15 oct-2020 and 31-oct-2020
 
-db.users.aggregate([
+db.users.aggregate(
+    [
+            {
+            $lookup: {
+                        from: "attendance",
+                        localField: "user_id",
+                        foreignField: "user_id",
+                        as: "attendance" 
+                    }
+             },
   {
-    $lookup: {from: "attendance",localField: "_id",foreignField: "userId",as: "attendance" }
-  },
-  {
-    $lookup: {from: "tasks",localField: "_id",foreignField: "assignedTo",as: "tasks"}
+    $lookup: 
+    {
+        from: "tasks",
+        localField: "user_id",
+        foreignField: "task_id",
+        as: "tasks"}
   },
   {
     $match: {
       $and: [
-        {"attendance.date": {$gte: ISODate("2020-10-15"),$lte: ISODate("2020-10-31")}},{"attendance.present": false},{"tasks.submitted": false}]}},
+        {task_due_date: {$gte: ISODate("2020-10-15"),$lte: ISODate("2020-10-31")}},{"attendance.present": false},{"tasks.submitted": false}]}},
   {
     $group: {_id: null,count: { $sum: 1 }}
 }])
